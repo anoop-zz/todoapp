9c769deedd92459fc283f139ec40ba273e10c4b3
anoopsahadevan <anoopsahadevan112@gmail.com>
2023-05-20 15:42:53 +0530

    modified:   App.js

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ToDoApp from "./components/ToDoApp";
-import ModalTest from "./components/ModalTest";
-import PropsTest from "./components/PropsTest";
 
 function App(props) {
   return <ToDoApp />;
